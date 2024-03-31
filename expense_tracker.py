fc4eb71e490120826a0b2b9a89f3c90d40efb8ff
Peng Wang <pengwangdeu@gmail.com>
2024-03-31 10:52:34 +0200

    expense_tracker.py

--- expense_tracker.py
+++ expense_tracker.py
@@ -1,13 +1,27 @@
 import sys
 import csv
+import pandas as pd
+from datetime import datetime
 
 def main():
     ...
 
 
-def valid_input():
+def open_or_create():
     if len(sys.argv)==1:
         name=input('Please provide a name for your file: ')
+        date=input('Specify the date yyyy/mm/dd ')  # can be changed as index
+        categories=input('Specify category: ')
+        amount=input('Please specify amount,e.g. 100, 24 ...: ')
+        data=np.array([[int(i) for i in list(amount.split(','))]])
+        df= pd.DataFrame(data=data, columns=list(categories.split(',')))
+        df['Date']=[date]
+        df.set_index('Date',inplace=True)
+        print(df)
+
+
+
+
 
     elif len(sys.argv)==2:
         try:

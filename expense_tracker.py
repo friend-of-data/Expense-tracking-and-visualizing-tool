4669a8f725370ce29d8dad09f5705d5d83596f9e
friend-of-data <141180603+friend-of-data@users.noreply.github.com>
2024-04-02 18:13:05 +0200

    Update expense_tracker.py

--- expense_tracker.py
+++ expense_tracker.py
@@ -2,11 +2,12 @@ import sys
 import csv
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 
 def main():
     df=open_or_create()
     df=total_diff(df)
-    bar_plot(df,'Total')
+    plothelper(df,'Date','Total')
 
 
 def open_or_create():
@@ -15,10 +16,11 @@ def open_or_create():
         date=input('Specify the date yyyy/mm/dd ')  # can be changed as index
         categories=input('Specify category: ')
         amount=input('Please specify amount,e.g. 100, 24 ...: ')
-        data=np.array([[int(i) for i in list(amount.split(','))]])
-        df= pd.DataFrame(data=data, columns=list(categories.split(',')))
-        df['Date']=[date]
-        df.set_index('Date',inplace=True)
+        row_1=[date]+[int(i) for i in list(amount.split(','))]
+        header=['Date']+list(categories.split(','))
+        data=np.array([row_1])
+        df= pd.DataFrame(data=data, columns=header)
+        df=df.set_index('Date')
         print(df)
         df.to_csv(name)
         return df
@@ -29,17 +31,20 @@ def open_or_create():
 
     elif len(sys.argv)==2:
         try:
-            df=pd.read_csv(sys.argv[2])
-            print(df.tail(3))
-            new_date=input('Specify the date yyyy/mm/dd: ')
-            new=input('Please provide new value for each column,e.g. 10,15,...: ')
-            new=[int(i) for i in list(new.split(','))]
-            new_row=[new_date]+new
-            with open('expense.csv','a', newline='') as file:
-                writer=csv.writer(file)
-                writer.writerow(new_row)
-            df=pd.read_csv(sys.argv[2])
-            return df
+            df=pd.read_csv(sys.argv[1])
+        except:
+            sys.exit('Please give the correct csv file name')
+        print(df.tail())
+        new_date=input('Specify the date yyyy/mm/dd: ')
+        new=input('Please provide new value for each column,e.g. 10,15,...: ')
+        new=[int(i) for i in list(new.split(','))]
+        new_row=[new_date]+new
+        with open(sys.argv[1],'a', newline='') as file:
+            writer=csv.writer(file)
+            writer.writerow(new_row)
+        df=pd.read_csv(sys.argv[1])
+        return df
+
 
     
             
@@ -57,11 +62,21 @@ def total_diff(df):
     ...
 
 
-def bar_plot(df,col):
-    plt.bar(df['Date'],df[col])
+def plothelper(df,xcol,ycol):
+    plt.subplot(1,2,1)
+    plt.bar(df[xcol],df[ycol])
     plt.xticks(rotation=45)
-    plt.plot(df['Date'], df[col].diff())
+    plt.title(f'{ycol}')
+
+    plt.subplot(1,2,2)
+    plt.plot(df[xcol], df[ycol].diff())
+    for xi,yi in zip(df[xcol][1:],df[ycol].diff()[1:]):
+        plt.text(xi,yi,str(yi),ha='right')
+    plt.xticks(rotation=45)
+    plt.title('Changes')
     plt.show()
+    
+    
     
 
 
